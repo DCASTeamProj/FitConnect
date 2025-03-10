68d2dad53e314e44770fed61108c5f08b24af6bd
Chapman <dustin.chapman@hrblock.com>
2025-03-10 09:48:14 -0500

    initial commit, component structure

---
+++ UI/FitConnectUI/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarLeftComponent } from './components/sidebar-left/sidebar-left.component';
+import { SidebarRightComponent } from './components/sidebar-right/sidebar-right.component';
+import { StoryComponent } from './components/story/story.component';
+import { PostComponent } from './components/post/post.component';
+import { ChatComponent } from './components/chat/chat.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    SidebarLeftComponent,
+    SidebarRightComponent,
+    StoryComponent,
+    PostComponent,
+    ChatComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
