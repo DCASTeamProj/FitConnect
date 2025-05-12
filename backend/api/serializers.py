af665c21bc79c0d0c26e0445aae9229bbc8d388c
Alvaro-Salvador <a_salvador@mail.fhsu.edu>
2025-05-12 19:14:13 +0200

    Update serializers.py

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,13 +1,27 @@
 from rest_framework import serializers
 from .models import User, Post, Comment
+from django.contrib.auth.password_validation import validate_password
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, min_length=8)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'bio', 'profile_picture']
+        fields = ['id', 'username', 'password', 'bio', 'profile_picture']
+
+    def validate_password(self, value):
+        validate_password(value)  # Usa validadores de Django (mayúsculas, números, etc.)
+        return value
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)  # Guarda la contraseña cifrada
+        user.save()
+        return user
 
 class PostSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)  # Muestra info del usuario en lugar de solo el ID
+    user = UserSerializer(read_only=True)
 
     class Meta:
         model = Post
